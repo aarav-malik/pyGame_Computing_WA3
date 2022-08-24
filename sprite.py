44941a29b3d4485a701f5cc2a2c7b24531902796
awaffle01 <95768353+awaffle01@users.noreply.github.com>
2022-08-24 22:47:00 +0800

    -

--- sprite.py
+++ sprite.py
@@ -14,7 +14,7 @@ class Player(pygame.sprite.Sprite):
         self.x = ppos[0]
         self.y = ppos[1]
         self.image = pygame.image.load("Graphics/cyborg {}/{}{}.png".format(self.state, self.state, self.stage))
-        self.image.set_colorkey(colours.white)
+        self.image.set_colorkey(colours.black)
         self.rect = self.image.get_rect(topleft=ppos)
         self.sprites.add(self)
 
@@ -32,5 +32,6 @@ class Player(pygame.sprite.Sprite):
         return touch
 
     def load(self):
-        self.surface.blit(self.image, (self.x, self.y), self.rect)
+        # pygame.draw.rect(self.surface,(255,255,255), self.rect,2)
+        self.surface.blit(self.image, (self.x, self.y))
         self.surface.blit(self.image, (self.x, self.y))
