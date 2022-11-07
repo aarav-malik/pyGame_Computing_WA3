643a82e7b454667664283b9210b52b80e1aa8828
awaffle01 <95768353+awaffle01@users.noreply.github.com>
2022-11-07 23:49:22 +0800

    -

--- collectible.py
+++ collectible.py
@@ -9,29 +9,8 @@ class Flask(pygame.sprite.Sprite):
         self.activated = False
         self.screen = screen
         self.sprites = []
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_01.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_02.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_03.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_04.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_05.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_06.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_07.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_09.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_10.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_11.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_12.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_13.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_14.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_15.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_16.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_17.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_18.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_19.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_20.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_21.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_22.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_23.png'))
-        self.sprites.append(pygame.image.load('Graphics/flask/frame_24.png'))
+        for x in range(25):
+            self.sprites.append(pygame.image.load('Graphics/flask/frame_{}.png'.format(x)))
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
         self.rect = self.image.get_rect(bottomleft=(self.x, self.y))
